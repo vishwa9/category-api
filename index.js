074d8405418bbd5a21ea6ecbebabed87ed8e47da
Vikrant Yadav <vikrant.y.22@gmail.com>
2016-07-24 13:51:51 +0530

    CORS headers added

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ require('./models')(wagner);
 //require('./config')(wagner);
 
 var app = express();
+
+app.use(function(req, res, next){
+  res.append('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.append('Access-Control-Allow-Credentials', 'true');
+  res.append('Access-Control-Allow-Methods', ['GET', 'OPTIONS', 'PUT', 'POST']);
+  res.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 wagner.invoke(require('./auth'),{ app:app});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
